e7747bf7503548a40b02cae3fcb7f18d4cb0e37c
Lysenkoyaroslav <49689611+Lysenkoyaroslav@users.noreply.github.com>
2020-03-28 23:20:17 +0200

    Update SecurityControllerCest.php

--- tests/api/SecurityControllerCest.php
+++ tests/api/SecurityControllerCest.php
@@ -1,8 +1,5 @@
 <?php
-
-
 namespace App\Tests\api;
-
 
 use App\Entity\Users;
 use App\Tests\ApiTester;
@@ -13,19 +10,14 @@ class SecurityControllerCest
     {
         ApiTester::createUserNoToken($I);
 
-
         $data = [
             'userName' => 'Davert',
             'password' => '12345',
             'email' => 'test@mail.com'
         ];
         $json = json_encode($data);
-
-
         $I->sendPOST('/login', $json);
-
         $apiToken=$I->grabFromRepository(Users::class, 'apiToken', array('userName' => 'Davert'));
-
         $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
         $I->seeResponseContains($apiToken);
     }
@@ -38,26 +30,21 @@ class SecurityControllerCest
             'email' => 'test@mail.com'
         ];
         $json = json_encode($data);
-
-
         $I->sendPOST('/login', $json);
-
         $I->seeResponseContains('"userName" and "password" are required fields!');
     }
 
     public function loginWrongData(ApiTester $I)
     {
         ApiTester::createUserNoToken($I);
+        
         $data = [
             'userName' => 'Davert',
             'password' => 'aaa12345',
             'email' => 'test@mail.com'
         ];
         $json = json_encode($data);
-
-
         $I->sendPOST('/login', $json);
-
         $I->seeResponseContains('Access denied!');
     }
 }
