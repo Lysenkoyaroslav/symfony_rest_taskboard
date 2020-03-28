03933488f0e5be841f96510430b58c44a7b3412d
Lysenkoyaroslav <49689611+Lysenkoyaroslav@users.noreply.github.com>
2020-03-28 23:23:04 +0200

    Update UsersControllerCest.php

--- tests/api/UsersControllerCest.php
+++ tests/api/UsersControllerCest.php
@@ -1,43 +1,33 @@
 <?php
-
-
 namespace App\Tests\api\UsersControllerTests;
-
 
 use App\Entity\Users;
 use App\Tests\ApiTester;
 
 class UsersControllerCest
 {
-
     public function getUserByIdViaAPI(ApiTester $I)
     {
-
         ApiTester::createAuthUser($I);
 
         $id = $I->grabFromRepository(Users::class, 'id', array('userName' => 'Davert'));
-
         $I->sendGET('/api/users/' . $id);
         $I->seeResponseCodeIs(200);
         $I->seeResponseIsJson();
-
     }
 
     public function getUserByIdNonexistentIdViaAPI(ApiTester $I)
     {
-
         ApiTester::createAuthUser($I);
 
         $id = -1;
         $I->sendGET('/api/users/' . $id);
         $I->seeResponseCodeIs(404);
-
     }
 
     public function getUsersViaAPI(ApiTester $I)
     {
         ApiTester::createAuthUser($I);
-
 
         $I->sendGET('/api/users');
         $I->seeResponseCodeIs(200);
@@ -54,13 +44,10 @@ class UsersControllerCest
             'email' => 'davert@codeception.com'
         ];
         $json = json_encode($data);
-
-
         $I->sendPOST('/api/signup', $json);
         $I->seeResponseCodeIs(\Codeception\Util\HttpCode::CREATED); // 201
         $I->seeResponseIsJson();
         $I->seeResponseContains('{"status":"ok"}');
-
     }
 
     public function requireFieldsErrorViaApi(ApiTester $I)
@@ -69,15 +56,10 @@ class UsersControllerCest
 
         $data = [
             'userName' => 'davert',
-
             'email' => 'davert@codeception.com'
         ];
         $json = json_encode($data);
-
-
         $I->sendPOST('/api/signup', $json);
-
         $I->seeResponseContains('Fill in required fields: userName, password, email!');
     }
-
 }
